8af44988bd7a0414fe7a850049185cf38d1fa02a
yigitbuyukkasap <yigitbyksp@gmail.com>
2022-05-02 20:37:47 +0300

    first commit

---
+++ AirlineSendAgent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AirlineSendAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
