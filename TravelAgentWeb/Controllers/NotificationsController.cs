20f4ac6ad3c4cc9801d1cb49a1b6cbc7cd665219
yigitbuyukkasap <yigitbyksp@gmail.com>
2022-05-03 19:50:05 +0300

    adding notificationcontroller post endpoint

--- TravelAgentWeb/Controllers/NotificationsController.cs
+++ TravelAgentWeb/Controllers/NotificationsController.cs
@@ -1,17 +1,46 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using TravelAgentWeb.Data;
+using TravelAgentWeb.Dtos;
 
 namespace TravelAgentWeb.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class NotificationsController: ControllerBase
+    public class NotificationsController : ControllerBase
     {
         private readonly TravelAgentDbContext _context;
 
         public NotificationsController(TravelAgentDbContext context)
         {
             _context = context;
+        }
+
+        [HttpPost]
+        public ActionResult FlightChanged(FlightDetailUpdateDto flightDetailUpdateDto)
+        {
+            Console.WriteLine($"Webhook Receieved from: {flightDetailUpdateDto.Publisher}");
+
+            var secretModel = _context.WebhookSecrets.FirstOrDefault(s =>
+                s.Publisher == flightDetailUpdateDto.Publisher
+                && s.Secret == flightDetailUpdateDto.Secret);
+
+            if (secretModel == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid Secret - Ignore Webhook");
+                Console.ResetColor();
+                return Ok();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Valid Webhook");
+                Console.WriteLine($"Old Price: {flightDetailUpdateDto.OldPrice}, New Price: {flightDetailUpdateDto.NewPrice}");
+                Console.ResetColor();
+                return Ok();
+            }
         }
     }
 }
