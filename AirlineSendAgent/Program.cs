9385f465680dc94c833695ae32bd5129da9ad760
yigitbuyukkasap <yigitbyksp@gmail.com>
2022-05-04 15:51:20 +0300

    data models and dbcontext

--- AirlineSendAgent/Program.cs
+++ AirlineSendAgent/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using AirlineSendAgent.App;
+using AirlineSendAgent.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +15,8 @@ namespace AirlineSendAgent
             var host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
             {
                 services.AddSingleton<IAppHost, AppHost>();
+                services.AddDbContext<SendAgentDbContext>(opt => opt.UseSqlServer
+                    (context.Configuration.GetConnectionString("AirlineConnection")));
 
             }).Build();
             host.Services.GetService<IAppHost>().Run();
