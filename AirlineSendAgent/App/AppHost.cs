541cb4a1f26a0997b590b1f0d83587f277b66b44
yigitbuyukkasap <yigitbyksp@gmail.com>
2022-05-04 15:41:49 +0300

    Adding Apphost --> Consuming MessageQueue and listining trigger

--- AirlineSendAgent/App/AppHost.cs
+++ AirlineSendAgent/App/AppHost.cs
@@ -1,4 +1,6 @@
 using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace AirlineSendAgent.App
 {
@@ -6,7 +8,32 @@ namespace AirlineSendAgent.App
     {
         public void Run()
         {
-            Console.WriteLine("Apphost - Run");
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(queue: queueName,
+                    exchange: "trigger",
+                    routingKey: ""
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+                Console.WriteLine("Listening on the message bus...");
+
+                consumer.Received += async (ModuleHandle, ea) =>
+                {
+                    Console.WriteLine("Event is triggered!");
+                };
+
+                channel.BasicConsume(queue: queueName,
+                    autoAck: true,
+                    consumer: consumer);
+                Console.ReadLine();
+            }
         }
     }
 }
